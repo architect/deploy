fa41d1c56c64ee4620284018b8da2ac99b04de06
Brian LeRoux <b@brian.io>
2019-07-19 12:36:09 -0700

    output the cdn url if you have one

--- src/sam/02-after/00-print-url.js
+++ src/sam/02-after/00-print-url.js
@@ -10,11 +10,14 @@ module.exports = function printURL({ts, pretty, stackname}, callback) {
     if (err) console.log(err)
     else if (Array.isArray(data.Stacks)) {
       let outs = data.Stacks[0].Outputs
+
+      let cdn = o=> o.OutputKey === 'CDN'
       let api = o=> o.OutputKey === 'API'
       let bucket = o=> o.OutputKey === 'BucketURL'
-      // output the api url falling back to the static bucket url
-      let url = outs.find(api) || outs.find(bucket)
-      if (url) pretty.url(url.OutputValue)
+
+      let url = outs.find(cdn) || outs.find(api) || outs.find(bucket)
+      if (url)
+        pretty.url(url.OutputValue)
     }
     callback()
   })
