c976a9ff3142d7a6de125d0fe83dc57da9df12fa
Ryan Block <rblock@gmail.com>
2019-08-13 08:44:20 -0700

    Adds more defensive nested accessors for cfn template macro

--- src/sam/macros/api-path.js
+++ src/sam/macros/api-path.js
@@ -6,28 +6,53 @@ module.exports = async function api({arc, cfn, stage}) {
 
   // @http path/stages
   if (arc.http) {
-    let APIs = cfn.Outputs.API.Value['Fn::Sub']
-    let API = APIs.findIndex(i => i.startsWith('https://') && i.includes('.execute-api.'))
-    APIs[API] = APIs[API].replace('staging', stage)
+    let outputsAPI =
+      cfn.Outputs &&
+      cfn.Outputs.API &&
+      cfn.Outputs.API.Value &&
+      cfn.Outputs.API.Value['Fn::Sub']
+    let API = outputsAPI.findIndex(i => i.startsWith('https://') && i.includes('.execute-api.'))
+    if (outputsAPI[API]) outputsAPI[API] = outputsAPI[API].replace('staging', stage)
+    else throw Error('Ouputs HTTP API not found in CloudFormation template')
   }
 
   // @ws path/stages
   if (arc.ws) {
     let getStage = i => typeof i === 'object' && i.stage
 
-    cfn.Resources.WebsocketStage.Properties.StageName = stage
+    let WSstageName =
+      cfn.Resources &&
+      cfn.Resources.WebsocketStage &&
+      cfn.Resources.WebsocketStage.Properties &&
+      cfn.Resources.WebsocketStage.Properties.StageName
+    if (WSstageName) WSstageName = stage
+    else throw Error('WebSocket stage name not found in CloudFormation template')
 
     let WSparam =
+      cfn.Resources &&
+      cfn.Resources.WebsocketParam &&
+      cfn.Resources.WebsocketParam.Properties &&
+      cfn.Resources.WebsocketParam.Properties.Value &&
       cfn.Resources.WebsocketParam.Properties.Value['Fn::Sub']
-    WSparam[WSparam.findIndex(getStage)].stage = stage
+    if (WSparam) WSparam[WSparam.findIndex(getStage)].stage = stage
+    else throw Error('WebSocket param stage not found in CloudFormation template')
 
     let WSconnParam =
+      cfn.Resources &&
+      cfn.Resources.WebsocketConnectionParam &&
+      cfn.Resources.WebsocketConnectionParam.Properties &&
+      cfn.Resources.WebsocketConnectionParam.Properties.Value &&
       cfn.Resources.WebsocketConnectionParam.Properties.Value['Fn::Sub']
-    WSconnParam[WSconnParam.findIndex(getStage)].stage = stage
+    if (WSconnParam) WSconnParam[WSconnParam.findIndex(getStage)].stage = stage
+    else throw Error('WebSocket connection param stage not found in CloudFormation template')
 
     let outputsWS =
+      cfn.Outputs &&
+      cfn.Outputs.WSS &&
+      cfn.Outputs.WSS.Value &&
       cfn.Outputs.WSS.Value['Fn::Sub']
-    outputsWS[outputsWS.findIndex(getStage)].stage = stage
+    if (outputsWS) outputsWS[outputsWS.findIndex(getStage)].stage = stage
+    else throw Error('Outputs WebSocket stage not found in CloudFormation template')
   }
 
   // TODO handle @cdn origin path here
