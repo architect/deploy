89338a13648f77182b92fc6e96ca126f63432e71
Ryan Block <rblock@gmail.com>
2020-06-08 19:58:45 -0700

    Respect @static spa in root proxy

--- src/sam/macros/_static.js
+++ src/sam/macros/_static.js
@@ -1,13 +1,23 @@
 /**
- * Adds ARC_STATIC_PREFIX to `get /` if @static prefix is defined
+ * Adds SPA, ARC_STATIC_PREFIX, etc. to `get /` (if defined)
  */
 module.exports = async function api(arc, cloudformation) {
   let cfn = cloudformation
+
+  // Prefix
   let prefixSetting = tuple => tuple[0] === 'prefix'
   let prefix = arc.static && arc.static.some(prefixSetting) && arc.static.find(prefixSetting)[1]
-
   if (cfn.Resources && cfn.Resources.GetIndex && prefix) {
     cfn.Resources.GetIndex.Properties.Environment.Variables['ARC_STATIC_PREFIX'] = prefix
+  }
+
+  // SPA (defaults to false if env var isn't set)
+  let spaSetting = tuple => tuple[0] === 'spa'
+  // findIndex instead of find so we don't mix up bools
+  let spa = arc.static && arc.static.some(spaSetting) && arc.static.findIndex(spaSetting)
+  let spaIsValid = arc.static[spa] && typeof arc.static[spa][1] === 'boolean'
+  if (cfn.Resources && cfn.Resources.GetIndex && spaIsValid) {
+    cfn.Resources.GetIndex.Properties.Environment.Variables['ARC_STATIC_SPA'] = arc.static[spa][1]
   }
 
   return cfn
