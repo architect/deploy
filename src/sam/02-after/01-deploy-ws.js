95178eceae70497b56e6f43d04d129672759e9c2
Ryan Block <rblock@gmail.com>
2024-01-22 14:15:54 -0800

    `aws-sdk` → `@aws-lite/apigatewayv2` in post-deployment WS patch

--- src/sam/02-after/01-deploy-ws.js
+++ src/sam/02-after/01-deploy-ws.js
@@ -1,5 +1,4 @@
 let series = require('run-series')
-let aws = require('aws-sdk')
 
 /**
  * API Gateway / CFN bug!
@@ -7,22 +6,17 @@ let aws = require('aws-sdk')
  * Hopefully we can yank this process out when they one day fix this
  */
 module.exports = function deployOldWebSocketAPI (params, callback) {
-  let { inventory, compat, region, stackname, stage: StageName } = params
+  let { aws, inventory, compat, stackname, stage: StageName } = params
   let { inv } = inventory
 
   if (inv.ws && compat.foundEarlierWS) {
-    let cloudformation = new aws.CloudFormation({ region })
-    let apig = new aws.ApiGatewayV2({ region })
     let ApiId
     let DeploymentId
 
     series([
       function describe (callback) {
-        cloudformation.describeStacks({
-          StackName: stackname
-        }, function done (err, cfn) {
-          if (err) callback(err)
-          else {
+        aws.cloudformation.DescribeStacks({ StackName: stackname })
+          .then(cfn => {
             try {
               let outs = cfn.Stacks[0].Outputs
               let ws = o => o.OutputKey === 'WSS'
@@ -34,53 +28,49 @@ module.exports = function deployOldWebSocketAPI (params, callback) {
             catch (err) {
               callback(ReferenceError('Cannot find WebSocket API in CloudFormation stack'))
             }
-          }
-        })
+          })
+          .catch(callback)
       },
 
       // We should only be responsible for an APIG deployment "patch" once, after that it's on AWS
       function maybeBailEarly (callback) {
-        function getDeployments (NextToken) {
-          let params = { ApiId }
-          if (NextToken) params.NextToken = NextToken
-          apig.getDeployments(params, function (err, result) {
-            if (err) callback(err)
-            else if (!result.Items) callback(Error('Cannot find WebSocket API deployments'))
+        aws.apigatewayv2.GetDeployments({ ApiId, paginate: true })
+          .then(result => {
+            if (!result.Items) callback(Error('Cannot find WebSocket API deployments'))
             else {
-              let patched = result.Items.find(i => i.Description && i.Description === 'arc_apig_cfn_patch')
-              let next = result.NextToken
+              let patched = result.Items.find(i => i?.Description === 'arc_apig_cfn_patch')
               if (patched) callback('cancel')
-              else if (next) getDeployments(next)
-              else callback() // We ran out of pages, now create the APIG patch
+              else callback()
             }
           })
-        }
-        getDeployments()
+          .catch(callback)
       },
 
       function deploy (callback) {
-        apig.createDeployment({
+        aws.apigatewayv2.CreateDeployment({
           ApiId,
           Description: 'arc_apig_cfn_patch'
-        },
-        function done (err, result) {
-          if (err) callback(err)
-          else if (!result || !result.DeploymentId) {
-            callback(Error('Failed to create WebSocket API deployment'))
-          }
-          else {
-            DeploymentId = result.DeploymentId
-            callback()
-          }
         })
+          .then(result => {
+            if (!result?.DeploymentId) {
+              callback(Error('Failed to create WebSocket API deployment'))
+            }
+            else {
+              DeploymentId = result.DeploymentId
+              callback()
+            }
+          })
+          .catch(callback)
       },
 
       function update (callback) {
-        apig.updateStage({
+        aws.apigatewayv2.UpdateStage({
           ApiId,
           StageName,
           DeploymentId
-        }, callback)
+        })
+          .then(() => callback())
+          .catch(callback)
       }
     ], function done (err) {
       if (err && err === 'cancel') callback()
