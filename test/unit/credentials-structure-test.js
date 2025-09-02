bd224855165a21779876d29a7a254edc4c174d81
Ryan Bethel <ryan.bethel@sanity.io>
2025-09-02 15:52:07 -0400

    fix tests

--- test/unit/credentials-structure-test.js
+++ test/unit/credentials-structure-test.js
@@ -1,48 +1,42 @@
 let test = require('tape')
-let awsLite = require('@aws-lite/client')
+let proxyquire = require('proxyquire')
 let inventory = require('@architect/inventory')
-let deploy = require('../../')
 
-test('Set up env', async t => {
-  t.plan(1)
-  awsLite.testing.enable()
-  t.ok(awsLite.testing.isEnabled(), 'AWS client testing enabled')
+let awsLiteArgs = []
+let mockAwsLite = (args) => {
+  awsLiteArgs.push(args)
+  return Promise.resolve({
+    cloudformation: {
+      DescribeStacks: () => Promise.reject({ message: 'Stack does not exist', statusCode: 400 }),
+      DescribeStackResources: () => Promise.resolve({ StackResources: [] }),
+    },
+  })
+}
+
+let deploy = proxyquire('../../', {
+  '@aws-lite/client': mockAwsLite,
 })
 
 test('deploy.sam credentials accepted', async t => {
-  t.plan(1)
+  t.plan(3)
   let inv = await inventory({
-    rawArc: '@app\ntest-app\n@http\nget /',
+    rawArc: '@app\ntest-app\n@static',
     deployStage: 'staging',
   })
-  try {
-    await deploy.sam({
-      credentials: {
-        accessKeyId: 'ASIATEST123456789',
-        secretAccessKey: 'testSecretKey123456789',
-        sessionToken: 'testSessionToken123456789',
-      },
-      inventory: inv,
-      isDryRun: true,
-      region: 'us-west-2',
-    })
-    t.pass('Deploy accepts credentials')
-  }
-  catch (err) {
-    // Check if this is the specific credential rejection error
-    if (err.message.includes('You must supply AWS credentials')) {
-      t.fail('Deploy rejects credentials')
-    }
-    else {
-      // Some other error occurred after credentials were accepted
-      t.pass('Deploy accepts credentials')
-    }
-  }
+  await deploy.sam({
+    credentials: {
+      accessKeyId: 'ASIATEST123456789',
+      secretAccessKey: 'testSecretKey123456789',
+      sessionToken: 'testSessionToken123456789',
+    },
+    inventory: inv,
+    isDryRun: true,
+    region: 'us-west-2',
+    shouldHydrate: false, // Skip hydration to avoid inventory structure issues
+  })
+  t.ok(awsLiteArgs[0].accessKeyId, 'accessKeyId is present')
+  t.ok(awsLiteArgs[0].secretAccessKey, 'secretAccessKey is present')
+  t.ok(awsLiteArgs[0].sessionToken, 'sessionToken is present')
+
 })
 
-test('Teardown', t => {
-  t.plan(1)
-  awsLite.testing.disable()
-  awsLite.testing.reset()
-  t.notOk(awsLite.testing.isEnabled(), 'AWS client testing disabled')
-})
